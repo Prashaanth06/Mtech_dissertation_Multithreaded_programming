ea3abdc6aaa60f51ae5d6ba41fbf8c2a4c54448c
Rajendran Malathy Prashaanth <ajuprashanth06@gmail.com>
2020-08-23 22:57:40 +0530

    after micropython code

--- programs/Micropython/non-multithreaded.py
+++ programs/Micropython/non-multithreaded.py
@@ -1,23 +1,23 @@
 import machine
 from machine import Pin,ADC
-import esp32
 from time import sleep
-import dht
 
-sensor = dht.DHT11(Pin(25))   # temperature sensor
 smoke = ADC(Pin(35))          # MQ-2 sensor
 manual_button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)   # Manual Press button
 flame = machine.Pin(27,machine.Pin.IN)   # Flame sensor
+led = machine.Pin(2,machine.Pin.OUT)      # LED
+buzzer = machine.Pin(4,machine.Pin.OUT)    # Buzzer 
+exhaust = machine.Pin(5,machine.Pin.OUT)    # Exhaust fan motor
 water = ADC(Pin(34))                   # water level sensor
 
-water_sensor = water.read()       # reading water level
-smoke_sensor = smoke.read()       # reading MQ2 sensor
-flame_value = flame.value()       # reading flame sensor
-man_press = manual_button.value()  # reading a manual interrupt
+buzzer_value = buzzer.value()     # buzzer reading
+led_state = led.value()           # LED reading the pin state
+exhaust_fan = exhaust.value()    # reading exhaust fan
 
 
-def water_level():
-    sleep(0.2)
+def water_level():                     # function for water level sensor
+    sleep(0.1)
+    water_sensor = water.read()       # reading water level
     print('Water level=%d' %water_sensor)
     if water_sensor >= 2000:
         print('Full tank water')
@@ -27,34 +27,55 @@ def water_level():
         print('Water is low level turn on the motor')
 
 
-def smoke_sens():
-    if man_press == 0:
-        sleep(0.2)
+def smoke_sens():                    # function for smoke sensor
+        sleep(0.1)
+        smoke_sensor = smoke.read()       # reading MQ2 sensor
         print('Smoke sensor value=%d ' % smoke_sensor)
 
-def flame_sensor():
-    if man_press == 0:
-        sleep(0.2)
+def flame_sensor():                  # function for flame sensor
+        sleep(0.1)
+        flame_value = flame.value()       # reading flame sensor
         print('Flame sensor value=%d ' % flame_value)   # Printing flame sensor value
 
-def manual_press(): 
-        sleep(0.2)
-        print('Manual Press button state=%d ' % man_press)   # Printing flame sensor value
-        print('Actuate things')
 
-def temp_sensor():   
-    if man_press == 0: 
-        sleep(0.3)
-        sensor.measure()
-        temp = sensor.temperature()
-        print('Temperature: %3.1f C' %temp)     # printing temperature value  
-        
+def manual_press():                  # function for manual press
+        man_press = manual_button.value()  # reading a manual interrupt
+        print('Manual button state=%d ' % man_press)   # Printing flame sensor value
+        print('Actuate things hardware interrupt is given')
+
+
+def actuation():                     # function for actuation unit
+        if manual_button.value() == 1 or smoke.read() >=1400 and flame.value()  == 0:
+            # LED on indication
+            led.value(1)
+            sleep(0.1)
+            led.value(0)
+            sleep(0.1)
+
+            # Buzzer indication
+            buzzer.value(1) 
+            sleep(0.1)
+            buzzer.value(0)
+            sleep(0.1)
+
+            # Exhaust fan
+            exhaust.value(1)
+
+            print('Fire has occured stay careful get out of here')
+
+        else:
+            led.value(0)
+            buzzer.value(0)
+            exhaust.value(0)
     
 while True:
-        manual_press()
+    actuation()
+    water_level()
+
+    if manual_button.value() == 0: 
         flame_sensor()
-        temp_sensor()
         smoke_sens()
-        water_level()
         print('')
-    
+    else:
+        manual_press()
+        print('')
---
+++ programs/Micropython/withthreads_micopython.py
@@ -0,0 +1,107 @@
+import machine
+from machine import Pin,ADC
+from time import sleep
+import _thread
+
+led = machine.Pin(2,machine.Pin.OUT)      # LED
+buzzer = machine.Pin(4,machine.Pin.OUT)    # Buzzer 
+exhaust = machine.Pin(5,machine.Pin.OUT)    # Exhaust fan motor
+manual_button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)   # Manual Press button
+smoke = ADC(Pin(35))                      # MQ-2 sensor
+flame = machine.Pin(27,machine.Pin.IN)      # Flame sensor
+
+mutex = _thread.allocate_lock()
+
+
+buzzer_value = buzzer.value()     # buzzer reading
+led_state = led.value()           # LED reading the pin state
+exhaust_fan = exhaust.value()    # reading exhaust fan
+
+
+def water_levelThread():                     # function for water level sensor
+    sleep(0.2)
+    print('')
+    water = ADC(Pin(34))                   # water level sensor
+    while True:
+        mutex.acquire()
+        water_sensor = water.read()       # reading water level
+        print('Water level=%d' %water_sensor)
+        if water_sensor >= 2000:
+            print('\nFull tank water')
+        elif water_sensor<= 1600 and water_sensor >= 550:
+            print('\nWater is in medium level')
+        elif water_sensor <= 500:
+            print('\nWater is low level turn on the motor')
+        mutex.release()
+
+
+def smoke_sensThread():                    # function for smoke sensor
+    sleep(0.2)
+    print('')
+    smoke_sensor = smoke.read()       # reading MQ2 sensor
+    while True:
+        mutex.acquire()
+        if manual_button.value() == 0:
+            print('Smoke sensor value=%d ' % smoke_sensor)
+        mutex.release()
+
+
+def manual_pressThread():                  # function for manual press
+    sleep(0.2)
+    print('')
+    #man_press = manual_button.value()  # reading a manual interrupt
+    while True:
+        mutex.acquire()
+        if manual_button.value() == 1:
+            #print('Manual button state=%d ' % man_press)   # Printing flame sensor value
+            print('\nActuate things hardware interrupt is given\n')
+        mutex.release()
+
+
+def flame_sensorThread():                  # function for flame sensor
+    sleep(0.2)
+    print('')
+    while True:
+        mutex.acquire()
+        if manual_button.value() == 0:
+            flame_value = flame.value()       # reading flame sensor
+            print('Flame sensor value=%d ' % flame_value)   # Printing flame sensor value
+        mutex.release()
+
+
+def actuationThread():                     # function for actuation unit
+    sleep(0.2)
+    print('')
+    while True:
+        mutex.acquire()
+        if manual_button.value() == 1 or smoke.read() >=1400 and flame.value()  == 0:
+            # LED on indication
+            led.value(1)
+            sleep(0.1)
+            led.value(0)
+            sleep(0.1)
+
+            # Buzzer indication
+            buzzer.value(1) 
+            sleep(0.1)
+            buzzer.value(0)
+            sleep(0.1)
+
+            # Exhaust fan
+            exhaust.value(1)
+
+            print('\nFire has occured stay careful get out of here')
+            print('')
+
+        else:
+            led.value(0)
+            buzzer.value(0)
+            exhaust.value(0)
+        mutex.release()
+
+
+_thread.start_new_thread(water_levelThread, ())
+_thread.start_new_thread(smoke_sensThread, ())
+_thread.start_new_thread(manual_pressThread, ())
+_thread.start_new_thread(flame_sensorThread, ())
+_thread.start_new_thread(actuationThread, ())
